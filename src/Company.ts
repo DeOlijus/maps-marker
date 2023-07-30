47aa1cf734291f1000b4b081d751350c879d5596
justin DeOliveira <deoli.justin@gmail.com>
2023-07-30 14:33:09 -0400

    create and configure the Company object

--- src/Company.ts
+++ src/Company.ts
@@ -1,1 +1,19 @@
-class Company {}
+import { faker } from '@faker-js/faker';
+
+export class Company {
+  companyName: string;
+  companyCatchprase: string;
+  companyLocation: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.companyName = faker.company.name();
+    this.companyCatchprase = faker.company.catchPhrase();
+    this.companyLocation = {
+      lat: faker.location.latitude(),
+      lng: faker.location.longitude(),
+    };
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { User } from './User';
+import { Company } from './Company';
 
 const user = new User();
+const company = new Company();
 console.log(`hi there`);
 console.log(user);
+console.log(company);
